a10b50fa5696f47f7eb03a86dc555de6d89ef08e
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-01 16:43:23 +0500

    variable and constant

---
+++ variable.php
@@ -0,0 +1,11 @@
+<?php 
+//echo "Hi this is the very first file and starter of the php course till end to become a good full stack developer"; 
+//$a="the very first lecture of php about variable or data types";
+//echo $a;
+
+$awais_kha ="the worpdress developer of metaa titans";
+
+echo $awais_kha . "<br>";
+echo $awais_kha . "<br>";
+?>
+nfnhfnhxgjxgmjhxmjh
