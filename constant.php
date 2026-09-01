7344e96fb102194ecc7b97688bf6a640ab81171c
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-01 17:46:37 +0500

    PHP Constant
    How we can define constant and how we can display the value of that constant variable in php

--- constant.php
+++ constant.php
@@ -1,3 +1,21 @@
 <?php 
 
+//This is how we can write and print the constant variable 
+// Once we declare any variable we can not change its value
+//here below in code the variable name is price and value is 30
+
+define("price",30);
+echo price;
+
+echo "<br>";
+
+define("age",22);
+echo age;
+
+echo "<br>";
+
+
+define("name","Junaid");
+echo name;
+
 ?>
