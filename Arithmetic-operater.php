546ee67ee47154f022c2e11d83a77d3cfcece2b7
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-02 11:07:04 +0500

    Arithmatic Operators

---
+++ Arithmetic-operater.php
@@ -0,0 +1,120 @@
+
+
+<?php
+//arithematic operters and its signs 
+// + addition
+// - subtraction
+// * Multiplication
+// ** Exponentiation
+// / Division
+// % modulus (Remainder)
+// ++ Increment
+// -- decrement
+
+
+
+
+
+//addition 
+
+$a = 32;
+$b = 32;
+
+$c = $a + $b;
+echo "$c";
+
+echo "<br>";
+
+//subtraction
+
+$a = 32;
+$b = 20;
+
+$c = $a - $b;
+echo "$c";
+
+
+
+
+echo "<br>";
+
+//Multiplication
+
+$a = 3;
+$b = 5;
+
+$c = $a * $b;
+echo "$c";
+
+
+
+echo "<br>";
+
+//divission
+
+$a = 30;
+$b = 5;
+
+$c = $a / $b;
+echo "$c";
+
+
+
+
+echo "<br>";
+
+//modulas or remainder
+
+$a = 30;
+$b = 4;
+
+$c = $a % $b; 
+echo "$c";
+
+
+
+echo "<br>";
+
+//Exponention or power of
+
+$a = 2;
+$b = 4;
+
+$c = $a ** $b; 
+echo "$c";
+
+
+echo "<br>";
+
+//inccrement
+
+$a = 2;
+$b = 4;
+$a++;
+echo "$a";
+
+
+
+echo "<br>";
+
+//decrement
+
+$a = 2;
+$b = 10;
+$b--;
+echo "$b";
+
+/*
+valuse of all the above calculation 
+64
+12
+15
+6
+2
+16
+3
+9
+*/
+
+
+?>
---
+++ Assignment-operators.php
@@ -0,0 +1,19 @@
+<?php
+// using the arithmatic operators with equll to (=)
+// +=
+//-=
+//*=
+
+//+=    Here in below calculation a will have the new value of adding a+b // a = a+b;
+// same for all the arithematic operators ;;
+
+
+$a = 10;
+$b = 20;
+$a += $b;
+
+echo $a;
+
+echo "<br>";
+
+?>
