cf867009ec6ea4d1907dbb0e801fa1dae5fffcf6
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-02 21:20:28 +0500

    comparison operators

---
+++ Comparison-operators.php
@@ -0,0 +1,32 @@
+<?php
+// comparison operrators is used fro comaprison between two or more than two variables 
+// these are the comparison opertaors 
+// ==
+//=== value equal and also check data type as well 
+//!= 
+//<>
+//>
+//<
+//>=
+//<=
+
+$a=20;
+$b=30;
+
+$c= $a>$b;
+echo $c;
+
+// it will return nothing becuase this is not true if it is true it will return 1 
+$a=20;
+$b=30;
+
+$c= $a<$b;
+echo $c;
+// it will return 1 becuase this is  true if it is true it will return 1  if not it will return nothing 
+
+
+//same for all the operators
+
+// In php we often use these operators with the if and ifelse statement
+
+?>
