015ea573498d39fab29b56b46e780aa79f3d5ef2
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-03 23:43:32 +0500

    while loop in php

---
+++ while-loop.php
@@ -0,0 +1,18 @@
+<?php
+// looops is usally used for printing data many times 
+// loops has three step 
+
+
+// initialization
+// condition
+// incremnet or decrement
+
+$a = 1;
+
+while($a <= 10){
+    echo "<br>this statment should print 10 times ";
+    $a=$a+1;
+}
+
+
+?>
