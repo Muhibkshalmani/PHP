7466e49bdfd4c9fc2fc567fe7a66b7088ea3c355
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-03 22:43:20 +0500

    ifelseif statment php

---
+++ ifelesif-statment.php
@@ -0,0 +1,40 @@
+<?php
+$per = 85;
+
+if($per >= 80 && $per <= 100){
+echo "the student is outstanding";
+}elseif($per>=60 && $per <= 70){
+    echo "the student is not bad";
+}elseif($per<=33 ){
+echo "the student is failed";
+}else{
+    echo"enter a valid number";
+}
+echo "<br>";
+
+$per = 65;
+
+if($per >= 80 && $per <= 100){
+echo "the student is outstanding";
+}elseif($per>=60 && $per <= 70){
+    echo "the student is not bad";
+}elseif($per<=33 ){
+echo "the student is failed";
+}else{
+    echo"enter a valid number";
+}
+echo "<br>";
+
+$per = 32;
+
+if($per >= 80 && $per <= 100){
+echo "the student is outstanding";
+}elseif($per>=60 && $per <= 70){
+    echo "the student is not bad";
+}elseif($per<=33 ){
+echo "the student is failed";
+}else{
+    echo"enter a valid number";
+}
+echo "<br>";
+?>
