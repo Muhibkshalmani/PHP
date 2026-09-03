a7e198b964af5291ffa23a5268e27c239db583aa
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-03 21:04:26 +0500

    If statment

---
+++ if-statment.php
@@ -0,0 +1,13 @@
+<?php
+
+// Here requirded age for the licenese is 18years ;
+// if junaid is age above 18 so it will print the statment below if not then it will print nothing  
+
+$junaid_age=30;
+
+if ($junaid_age > 18 ) {
+echo "yes he is eligible for the driving license ";
+}
+
+
+?>
