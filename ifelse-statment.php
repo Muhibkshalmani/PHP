77cb7d18c8cf769469d3fca8203523a3382900f8
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-03 21:52:36 +0500

    if else statement

---
+++ ifelse-statment.php
@@ -0,0 +1,29 @@
+<?php
+// if else statement is use when for both if condition is true then print if statemnt if not then else statment;
+
+$Awais_age = 25;
+
+if($Awais_age == 25){
+
+echo"yes the awais is 25 years old";
+}else{
+
+echo"<br> No awais is not 25 years old";
+
+}
+
+echo "<br>";
+
+$mubeen_age = 30;
+
+if($mubeen_age == 25){
+
+echo"yes  mubeen is 25 years old";
+
+}else{
+
+echo" No Mubeen is not 25 years old";
+
+}
+
+?>
