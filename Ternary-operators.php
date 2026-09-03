aa002e4024256dc0a3349473a08a01f4615eed7d
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-03 23:18:29 +0500

    ternary operators in php

---
+++ Ternary-operators.php
@@ -0,0 +1,28 @@
+<?php
+
+//Ternary operators are same workring like if else statemnet or condition but there is defference we can onlu pass a single statemnet here o ternary
+
+
+
+
+$jay_age = 25;
+$jay_age>=20? $jay= "yes":$jay="Nope";
+
+echo $jay;
+echo "<br>";
+
+$jay_age = 16;
+$jay_age>=20? $jay= "yes":$jay="Nope";
+
+echo $jay;
+echo "<br>";
+
+
+
+// printed  values  of the above code
+
+
+// yes for 1 
+// nope for 2
+
+?>
