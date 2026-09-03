006e40feffccb42e35fdff708c45f684a4836786
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-03 23:09:07 +0500

    swtich statemnet in php

---
+++ swtich-statment.php
@@ -0,0 +1,46 @@
+<?php
+// it is likely the ifelseif statment and almost it works the same way but the syntax is a bit change
+
+$month =5;
+switch($month){
+    case 1:
+        echo"the starting month is jan";
+    break;
+    case 2:
+        echo"the starting month is feb";
+    break;
+    case 3:
+        echo"the starting month is march";
+    break;
+    case 4:
+        echo"the starting month is april";
+    break;
+    case 5:
+        echo"the starting month is may";
+    break;
+    case 6:
+        echo"the starting month is jun";
+    break;
+    case 7:
+        echo"the starting month is july";
+    break;
+    case 8:
+        echo"the starting month is august";
+    break;
+    case 9:
+        echo"the starting month is september";
+    break;
+    case 10:
+        echo"the starting month is october";
+    break;
+    case 11:
+        echo"the starting month is november";
+    break;
+    case 12:
+        echo"the starting month is december";
+    break;
+    default:
+    echo "enter a valid month";
+    break;
+}
+?>
