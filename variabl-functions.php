bcf1905f344b88d562581a0eaf5eb05d2dd81687
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-04 22:56:35 +0500

    variable function in php

---
+++ variabl-functions.php
@@ -0,0 +1,17 @@
+<?php
+
+
+// this is how we can write a varible function and how we can call it 
+
+
+function variable(){
+
+echo "hey this is the php variable function";
+
+}
+
+$varfunc = "variable";
+
+echo $varfunc();
+
+?>
