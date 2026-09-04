ea9cfca0f2bc16ecbc801d9b9fb6ba54ec2f12aa
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-04 10:31:47 +0500

    functions in php

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+// functions is used for a specific purpose when we want to display a specific code many time in site then we put them in a function and then call the fucntion there where we need
+function hello(){
+    echo "Welcome to the site <br>";
+}
+
+hello();
+hello();
+hello();
+?>
