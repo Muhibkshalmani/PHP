17faa870609458498bd7b56b671e85707bc07799
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-04 22:13:48 +0500

    functions in php

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-// functions is used for a specific purpose when we want to display a specific code many time in site then we put them in a function and then call the fucntion there where we need
+// // functions is used for a specific purpose when we want to display a specific code many time in site then we put them in a function and then call the fucntion there where we need
 function hello(){
     echo "Welcome to the site <br>";
 }
@@ -7,4 +7,35 @@ function hello(){
 hello();
 hello();
 hello();
+
+
+
+// // function with returning value
+
+function name ($fname, $lname){
+
+$fullname = "$fname  $lname";
+return $fullname ;
+
+}
+echo name("Muhib","jaan" . "<br>");
+echo name("Muhib","jaan" . "<br>");
+echo name("Muhib","jaan" . "<br>");
+echo name("Muhib","jaan" . "<br>");
+
+
+//  how to pass or put php function arguments as  a refference 
+
+
+function prof(&$porfession){
+$porfession .= "  yes there is me";
+
+}
+
+$rank =  " Hello this is mubi and Iam a  web designer and now learning php";
+ prof( $rank);
+
+echo $rank;
+
+
 ?>
