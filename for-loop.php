d33a8ed9c4dff1bad2dd63aeec42445a53c02577
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-04 09:36:50 +0500

    for loop in php

---
+++ for-loop.php
@@ -0,0 +1,13 @@
+<?php
+// In for loop initialization ,condition,increment or decrement should be done in a single line like
+
+for($a=1; $a <=10;  $a++){
+
+echo $a . ") this is statment should print 10 times with the help of for loop <br>";
+
+
+}
+
+// this is how for syntax is and how it should be written 
+
+?>
