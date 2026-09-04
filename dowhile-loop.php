09fc824d0c2b61dd7220bd2a11b93d7c2c25eabf
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-04 09:19:26 +0500

    do while loop in php

--- dowhile-loop.php
+++ dowhile-loop.php
@@ -1,3 +1,15 @@
 <?php
+// Do while loop .. first we have to initialize the value then statment and increment or decrement then condition
+// it works the same but just the synatax is a bit change 
+
+
+$a =1;
+
+do{
+    echo "this statment should be print more than 10 time with the help of do while loop <br>";
+    $a++;
+}while($a <=12);
+
+
 
 ?>
