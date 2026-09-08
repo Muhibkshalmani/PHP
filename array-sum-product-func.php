4e81d4aacc6f44bce1187c7582db5d2c2accb32e
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-08 10:41:00 +0500

    array sum and array product functions in php

---
+++ array-sum-product-func.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = [10,20,30,40,50];
+$b = array_sum($a);
+
+print_r($b);
+
+
+echo "<br>";
+
+$c = array_product($a);
+print_r($c);
+
+// to add integer values of an array or multiple theme..these functions are often use for that
+
+?>
