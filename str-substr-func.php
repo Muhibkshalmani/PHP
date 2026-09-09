1416a12e8f7e61217b06762d3af8d2b1015321cf
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-09 23:54:25 +0500

    sub str functions in php

---
+++ str-substr-func.php
@@ -0,0 +1,10 @@
+<?php
+// this function is used to cut a string from a string values....
+
+$php = "I love php , php is more easy and understable langauge , php is a demanded language";
+
+echo substr($php,10,);
+
+// substr ( varibal , start, end or length)
+
+?>
