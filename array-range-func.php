b57a105e44b575233b555016c890e58b602e5d81
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-09 11:15:13 +0500

    array range function in php

---
+++ array-range-func.php
@@ -0,0 +1,21 @@
+<?php
+// array range function is use for to make a numeric array like I will give give 2 parameter start and end adn it will return 
+// an array of that all numeric valus like 
+
+$range_testing = range(1,10);
+
+echo "<pre>";
+print_r ($range_testing);
+echo "</pre>";
+
+// it return all the numberr or degit from 1-10;
+
+$range_testing = range("a","z");
+
+echo "<pre>";
+print_r ($range_testing);
+echo "</pre>";
+
+
+
+?>
