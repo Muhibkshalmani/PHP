e09e7970a0cee379570d599491c67e59886706a1
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-10 23:00:19 +0500

    string tim and chop functions  in php

---
+++ str-trim-chop-function.php
@@ -0,0 +1,15 @@
+<?php
+// trim function is use for triming any string value in php but it mostly used for removing space
+// chop function is also work the same as like ltrim it will trim from the right 
+
+
+
+$a = "Mashaal";
+
+echo trim($a, "al");
+
+echo "<br>";
+
+echo trim($a, "al");
+
+?>
