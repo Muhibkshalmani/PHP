578ef7196fb6b5d9fdb430960dccf27e13411821
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-10 22:23:34 +0500

    string reverse adn string shuffle function in php

---
+++ str-rev-shuffle-func.php
@@ -0,0 +1,14 @@
+<?php
+// str revers function works to revers a a strig value 
+// str shuffle function work for a random order of a string value 
+
+
+$a = "Muhib";
+
+echo strrev($a);
+
+echo "<br>";
+
+echo str_shuffle($a);
+
+?>
