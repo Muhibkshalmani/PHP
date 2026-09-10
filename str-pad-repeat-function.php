ca8f8d429b9fa066258b772c898960abe38088f7
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-10 22:39:09 +0500

    string pad and repeat functions in php

---
+++ str-pad-repeat-function.php
@@ -0,0 +1,19 @@
+<?php
+// str pad function is use to add charracter or any value to a existing value to make that lenght as much as you want or add padding 
+// str repeat use for to repeat a string value simply 
+
+
+$a = "Maa";
+
+echo str_pad($a, 8,"shaal");
+
+
+echo "<br>";
+
+
+$a = "Muhib";
+
+echo str_repeat($a . "<br>", 15);
+
+
+?>
