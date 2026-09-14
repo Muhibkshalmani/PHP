90096164fc4853bb2c95bb7b58fc00627aaa5d55
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-14 23:50:04 +0500

    math floor round ceil abs functions in php

---
+++ math-ceil-floor-round-abs-func.php
@@ -0,0 +1,28 @@
+<?php
+// these funciton are spicifically use for decimal or float values 
+
+$Rating = 4.3;
+
+echo round($Rating) . "<br>";
+
+$Rating = 4.8;
+
+echo round($Rating) . "<br>";
+
+$Rating = 4.8;
+
+echo ceil($Rating) . "<br>";
+
+$Rating = 4.8;
+
+echo floor($Rating) . "<br>";
+
+// abs funntion will provide the absolute value it will smply rmeove the plus or minus icon ..
+
+$Rating = -6.3;
+
+echo abs($Rating) . "<br>";
+
+
+
+?>
