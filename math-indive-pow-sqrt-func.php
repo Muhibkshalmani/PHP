04a1b395f90fb317ec1f021489c6a510b71d23aa
Muhibkshalmani <muhibk.shalmani@gmail.com>
2026-09-17 09:41:44 +0500

    math indiv pow and sqrt function in php

---
+++ math-indive-pow-sqrt-func.php
@@ -0,0 +1,24 @@
+<?php
+// all these functions are used for calculation or maths 
+
+echo intdiv(8,4);
+
+echo "<br>";
+
+
+echo pow(2,4);
+
+echo "<br>";
+
+
+
+
+echo "<br>";
+
+
+echo sqrt(16);
+
+
+
+
+?>
